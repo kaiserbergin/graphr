874b03937082ccf571f725b1420185726bd08cce
Donovan Bergin <kaiserbergin@gmail.com>
2021-08-20 23:51:56 -0500

    bye bye warnings

--- Graphr.Neo4j/Configuration/NeoDriverConfigurationSettings.cs
+++ Graphr.Neo4j/Configuration/NeoDriverConfigurationSettings.cs
@@ -2,9 +2,9 @@ namespace Graphr.Neo4j.Configuration
 {
     public class NeoDriverConfigurationSettings
     {
-        public string Url { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string? Url { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
         public bool IsDebugLoggingEnabled { get; set; }
         public bool IsTraceLoggingEnabled { get; set; }
     }
