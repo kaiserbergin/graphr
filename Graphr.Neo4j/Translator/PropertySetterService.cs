945e951e29bae9633e89fa80576a2ff367159697
Donovan Bergin <kaiserbergin@gmail.com>
2021-08-25 13:53:41 -0500

    More typey typey

--- Graphr.Neo4j/Translator/PropertySetterService.cs
+++ Graphr.Neo4j/Translator/PropertySetterService.cs
@@ -10,37 +10,38 @@ namespace Graphr.Neo4j.Translator
     {
         internal static void SetPropertyValue(PropertyInfo propertyInfo, object target, object neoProp)
         {
-            if (propertyInfo.PropertyType == typeof(DateTime))
-                propertyInfo.SetValue(target, neoProp.As<DateTime>());
-            else if (propertyInfo.PropertyType == typeof(DateTimeOffset))
-                propertyInfo.SetValue(target, neoProp.As<DateTimeOffset>());
-            else if (propertyInfo.PropertyType == typeof(TimeSpan))
-                propertyInfo.SetValue(target, neoProp.As<TimeSpan>());
-            else if (IsGenericIEnumerable(propertyInfo.PropertyType))
-                SetIEnumerableProperty(propertyInfo, target, neoProp);
-            else
-                propertyInfo.SetValue(target, neoProp);
+            var convertedNeoProp = ConvertToClrType(propertyInfo.PropertyType, neoProp);
+            propertyInfo.SetValue(target, convertedNeoProp);
         }
 
-        private static void SetIEnumerableProperty(PropertyInfo propertyInfo, object target, object neoProp)
+        private static object? ConvertToClrType(Type type, object? neoProp)
         {
-            var list = CreateListFromNeo(propertyInfo.PropertyType, neoProp);
-            
-            propertyInfo.SetValue(target, list);
+            if (IsGenericIEnumerable(type))
+                return CreateListFromNeo(type, neoProp);
+
+            if (type == typeof(DateTime))
+                return neoProp.As<DateTime>();
+            if (type == typeof(DateTimeOffset))
+                return neoProp.As<DateTimeOffset>();
+            if (type == typeof(TimeSpan))
+                return neoProp.As<TimeSpan>();
+
+            return neoProp;
         }
 
-        private static object CreateListFromNeo(Type type, object neoProp)
+        private static object CreateListFromNeo(Type type, object? neoProp)
         {
             var genericType = type.GetGenericArguments()[0];
-            
+
             var genericListType = typeof(List<>).MakeGenericType(genericType) ?? throw new NullReferenceException($@"Could not create generic {genericType} typed list from.");
             var instance = (IList) Activator.CreateInstance(genericListType)!;
 
             if (neoProp == null) return instance;
-            
+
             foreach (var item in (IList) neoProp)
             {
-                instance.Add(Convert.ChangeType(item, genericType));
+                var convertedItem = ConvertToClrType(genericType, item);
+                instance.Add(Convert.ChangeType(convertedItem, genericType));
             }
 
             return instance;
