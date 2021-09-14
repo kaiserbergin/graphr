8ebdad62c5761417c7b50149fd36aa639e513c4a
Donovan Bergin <kaiserbergin@gmail.com>
2021-09-14 06:30:37 -0500

    Adding cancellation token support (limited as the driver doesn't)

--- Graphr.Neo4j/Graphr/INeoGraphr.cs
+++ Graphr.Neo4j/Graphr/INeoGraphr.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Neo4j.Driver;
 
@@ -6,17 +7,17 @@ namespace Graphr.Neo4j.Graphr
 {
     public interface INeoGraphr
     {
-        Task<List<T>> ReadAsAsync<T>(string query) where T : class, new();
-        Task<List<T>> ReadAsAsync<T>(string query, object parameters) where T : class, new();
-        Task<List<T>> ReadAsAsync<T>(string query, IDictionary<string, object> parameters) where T : class, new();
-        Task<List<T>> ReadAsAsync<T>(Query query) where T : class, new();
-        Task<List<T>> WriteAsAsync<T>(string query) where T : class, new();
-        Task<List<T>> WriteAsAsync<T>(string query, object parameters) where T : class, new();
-        Task<List<T>> WriteAsAsync<T>(string query, IDictionary<string, object> parameters) where T : class, new();
-        Task<List<T>> WriteAsAsync<T>(Query query) where T : class, new();
-        Task WriteAsync(Query query);
-        Task WriteAsync(string query);
-        Task WriteAsync(string query, object parameters);
-        Task WriteAsync(string query, Dictionary<string, object> parameters);
+        Task<List<T>> ReadAsAsync<T>(string query, CancellationToken cancellationToken = default) where T : class, new();
+        Task<List<T>> ReadAsAsync<T>(string query, object parameters, CancellationToken cancellationToken = default) where T : class, new();
+        Task<List<T>> ReadAsAsync<T>(string query, IDictionary<string, object> parameters, CancellationToken cancellationToken = default) where T : class, new();
+        Task<List<T>> ReadAsAsync<T>(Query query, CancellationToken cancellationToken = default) where T : class, new();
+        Task<List<T>> WriteAsAsync<T>(string query, CancellationToken cancellationToken = default) where T : class, new();
+        Task<List<T>> WriteAsAsync<T>(string query, object parameters, CancellationToken cancellationToken = default) where T : class, new();
+        Task<List<T>> WriteAsAsync<T>(string query, IDictionary<string, object> parameters, CancellationToken cancellationToken = default) where T : class, new();
+        Task<List<T>> WriteAsAsync<T>(Query query, CancellationToken cancellationToken = default) where T : class, new();
+        Task WriteAsync(Query query, CancellationToken cancellationToken = default);
+        Task WriteAsync(string query, CancellationToken cancellationToken = default);
+        Task WriteAsync(string query, object parameters, CancellationToken cancellationToken = default);
+        Task WriteAsync(string query, Dictionary<string, object> parameters, CancellationToken cancellationToken = default);
     }
 }
